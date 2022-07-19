738ab8ab8fc06c9c3d05e129a7fd328d222927af
Albert I <kras@raphielgang.org>
2022-07-20 00:51:07 +0530

    sdm710-common: Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- sdm710.mk
+++ sdm710.mk
@@ -368,6 +368,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Improve performance
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Telephony
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
