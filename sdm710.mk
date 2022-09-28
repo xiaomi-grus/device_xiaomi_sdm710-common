e8bc6c24d00062eb8117b4cb5bbc32f4e7a431b1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:42:16 +0200

    sdm710-common: Remove non-existing or implicitly included IPACM packages
    Change-Id: Iae06f09e101b237187638e76a6dd903ecdc2ca4f

--- sdm710.mk
+++ sdm710.mk
@@ -223,9 +223,7 @@ PRODUCT_COPY_FILES += \
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # IRQ balance
 PRODUCT_COPY_FILES += \
