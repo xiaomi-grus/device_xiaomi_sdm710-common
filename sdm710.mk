a4d3bc6ddaa4878dff3054444e41c289ed98e564
RDS-007 <ramandeep392@gmail.com>
2022-12-05 17:12:58 +0530

    sdm710-common: libmegface -> libmegface_xiaomi

--- sdm710.mk
+++ sdm710.mk
@@ -129,7 +129,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    libmegface \
+    libmegface_xiaomi \
     libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
